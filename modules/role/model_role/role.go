e687bb29c11ca12cb3e6bdc52eb42bbd8f624979
convitnhodev <hungthai10a1@gmail.com>
2022-08-05 17:16:44 +0700

    ve nha lam tiep

--- modules/role/model_role/role.go
+++ modules/role/model_role/role.go
@@ -9,7 +9,7 @@ type Role struct {
 	RoleCode        string       `json:"role_code" gorm:"column:role_code;primary_key"`
 	RoleName        string       `json:"name_role" gorm:"column:name_role"`
 	DepartmentId    int          `json:"department_id" gorm:"column:dept_id"`
-	Permissions     []Permission `json:"permissions" gorm:"many2many:role_permission;joinForeignKey:role_code;joinReferences:permission_code"`
+	Permissions     []Permission `json:"permissions" gorm:"many2many:role_permission;joinForeignKey:role_code;References:joinReferences:permission_code"`
 }
 
 func (r Role) TableName() string {
